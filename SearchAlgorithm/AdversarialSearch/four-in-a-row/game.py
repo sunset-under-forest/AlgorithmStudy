c17f7eab188eb085975fdf319ea7aed97c36fc7d
0ne_bey0nd <bey0nd@Art.com>
2023-04-07 22:57:13 +0800

    AI算法学习项目v0.0.7 新增connect4_min_max_search（默认包含alpha_beta剪枝，相关函数和功能已基本实现，但是表现效果一般，并不是很聪明，ai还是会输）

--- SearchAlgorithm/AdversarialSearch/four-in-a-row/game.py
+++ SearchAlgorithm/AdversarialSearch/four-in-a-row/game.py
@@ -1,6 +1,8 @@
 # connect four game
 import platform
+import random
 from os import system
+from math import inf as infinity
 
 import numpy as np
 
@@ -98,16 +100,110 @@ class ConnectFour:
         else:
             self.player = 1
 
+    def get_valid_locations(self):
+        valid_locations = []
+        for col in range(self.cols):
+            if self.is_valid_location(col):
+                valid_locations.append(col)
+        return valid_locations
+
+    def get_current_score(self):
+        score = 0
+        center_array = [int(i) for i in list(self.board[:, self.cols // 2])]  # center column
+        center_count = center_array.count(self.player)
+        score += center_count * 3  # center column is more important
+
+        # horizontal
+        for r in range(self.rows):
+            row_array = [int(i) for i in list(self.board[r, :])]  # go through each row
+            for c in range(self.cols - 3):
+                window = row_array[c:c + 4]
+                score += self.evaluate_window(window)
+
+        # vertical
+        for c in range(self.cols):
+            col_array = [int(i) for i in list(self.board[:, c])]  # go through each column
+            for r in range(self.rows - 3):
+                window = col_array[r:r + 4]
+                score += self.evaluate_window(window)
+
+        # positive sloped diagonal
+        for r in range(self.rows - 3):
+            for c in range(self.cols - 3):
+                window = [self.board[r + i][c + i] for i in range(4)]
+                # go through each diagonal from top left to bottom right
+                score += self.evaluate_window(window)
+
+        # negative sloped diagonal
+        for r in range(self.rows - 3):
+            for c in range(self.cols - 3):
+                window = [self.board[r + 3 - i][c + i] for i in range(4)]
+                # go through each diagonal from bottom left to top right
+                score += self.evaluate_window(window)
+
+        return score
+
+    def evaluate_window(self, window):
+        score = 0
+        opp_piece = self.player * -1
+
+        if window.count(self.player) == 4:
+            score += 100
+        elif window.count(self.player) == 3 and window.count(0) == 1:
+            score += 5
+        elif window.count(self.player) == 2 and window.count(0) == 2:
+            score += 2
+
+        if window.count(opp_piece) == 3 and window.count(0) == 1:
+            score -= 4
+
+        return score
+
+    def is_game_over(self):
+        return self.winning_move(self.player) or len(self.get_valid_locations()) == 0
+
+    # minimax algorithm
+    def min_max(self, depth, maximizingPlayer=1, alpha=-infinity, beta=infinity):
+        valid_locations = self.get_valid_locations()
+        is_terminal = self.is_game_over()
+        if depth == 0 or is_terminal:
+            return [None, self.get_current_score()]
+
+        best = [None, -infinity] if maximizingPlayer else [None, infinity]
+
+        for col in valid_locations:
+            row = self.get_next_open_row(col)
+            self.drop_piece(col)
+            score = self.min_max(depth - 1, -1 * maximizingPlayer, alpha, beta)[1]
+            self.board[row][col] = 0
+            if maximizingPlayer:
+                if score > best[1]:
+                    best = [col, score]
+                alpha = max(alpha, score)
+                if alpha >= beta:
+                    break
+            else:
+                if score < best[1]:
+                    best = [col, score]
+                beta = min(beta, score)
+                if alpha >= beta:
+                    break
+
+        return best
+
     def play_game(self):
         game_over = False
-
         while not game_over:
-            # Player 1 input
+
+            # col, minimax_score = self.min_max(4, self.player)
+            # # show the minimax score and player
+            # print("Player", "O" if self.player == 1 else "X", "minimax score:", minimax_score)
             if self.player == 1:
-                col = int(input("Player 1 (O) make your selection (0-6): "))
-            # Player 2 input
+                col = int(input("Player 1 (O) make your selection (0-" + str(self.cols - 1) + "):"))
             else:
-                col = int(input("Player 2 (X) make your selection (0-6): "))
+                col, minimax_score = self.min_max(7, self.player)
+                print("Player 2 (X) make your selection (0-" + str(self.cols - 1) + "):", col)
+
 
             if self.drop_piece(col):
                 if self.winning_move(self.player):
