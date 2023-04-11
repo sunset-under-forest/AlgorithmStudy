e735e1b4f37225a612e505aa70cc91153fda55d9
0ne_bey0nd <bey0nd@Art.com>
2023-04-11 23:19:37 +0800

    AI算法学习项目v0.0.11 背包问题基本完成，加入性能测试，1000代内可以达到理论最优解，且性能优于蛮力，代码整理中

--- GeneticAlgorithm/Knapsack/Knapsack.py
+++ GeneticAlgorithm/Knapsack/Knapsack.py
@@ -38,7 +38,7 @@ def chromosome_encoding(individual_size: int) -> np.ndarray:
 # 2. 设置算法参数
 
 # 种群大小
-POPULATION_SIZE = 100
+POPULATION_SIZE = 1000
 # 迭代次数
 ITERATION_TIMES = 1000
 # 交叉概率
@@ -68,10 +68,7 @@ def fitness_function(individual: Individual) -> Union[int, ndarray]:
     适应度函数
     :param individual: 个体
     """
-    if individual.weight > KNAPSACK_CAPACITY:
-        return 0
-    else:
-        return individual.value
+    return individual.value if individual.weight <= KNAPSACK_CAPACITY else 0
 
 
 # 5. 筛选亲本（轮盘赌）
@@ -86,50 +83,114 @@ def calculate_probability(population: list) -> np.ndarray:
     return probability
 
 
-def select(population: list) -> list:
+def roulette_wheel_selection(population: list, parents_num: int = 0) -> list:
     """
-    根据适应度计算个体被选中的概率
+    轮盘赌选择
     :param population: 种群
+    :param parents_num: 选择的父母个数
     """
+    parents_num = len(population) if parents_num == 0 else parents_num
     probability = calculate_probability(population)
-    parents = np.random.choice(population, size=len(population), replace=True, p=probability)
+    parents = np.random.choice(np.arange(len(population)), size=parents_num, replace=True, p=probability)
+    parents = [population[i] for i in parents]
     return parents
 
 
 # 6. 繁殖后代（交叉、变异）
 # 交叉
-def crossover(parent1: Individual, parent2: Individual) -> Individual:
+def crossover(parent1: Individual, parent2: Individual,
+              crossover_probability: float = 1) -> Individual:
     """
-    单点交叉
+    交叉
     :param parent1: 亲本1
     :param parent2: 亲本2
+    :param crossover_probability: 交叉概率
     """
-    if np.random.rand() < CROSSOVER_PROBABILITY:
-        # 选择交叉点
-        crossover_point = np.random.randint(0, len(parent1.chromosome))
-        # 交叉
-        child_chromosome = np.concatenate((parent1.chromosome[:crossover_point], parent2.chromosome[crossover_point:]))
-        child = Individual(child_chromosome)
-        return child
+    if np.random.rand() < crossover_probability:
+        point = np.random.randint(0, len(parent1.chromosome))
+        child = Individual(np.concatenate((parent1.chromosome[:point], parent2.chromosome[point:])))
     else:
-        return parent1
+        child = Individual(parent1.chromosome)
+    return child
 
 
 # 变异
-def mutation(individual: Individual) -> Individual:
+def mutation(individual: Individual, mutation_probability: float = 1) -> Individual:
     """
     变异
     :param individual: 个体
+    :param mutation_probability: 变异概率
     """
     for gene in range(len(individual.chromosome)):
-        if np.random.rand() < MUTATION_PROBABILITY:
-            individual.chromosome[gene] ^= 1 # 异或运算，相当于取反
+        if np.random.rand() < mutation_probability:
+            individual.chromosome[gene] ^= 1  # 异或运算，相当于取反
     return individual
 
 
-if __name__ == '__main__':
+# 7. 选择下一代
+def next_generation(population: list, crossover_probability: float, mutation_probability: float) -> list:
+    """
+    选择下一代
+    :param population: 种群
+    :param crossover_probability: 交叉概率
+    :param mutation_probability: 变异概率
+    """
+    parents = roulette_wheel_selection(population)
+    next_generation = []
+    for i in range(0, len(parents), 2):
+        parent1 = parents[i]
+        parent2 = parents[i + 1]
+        child1 = crossover(parent1, parent2, crossover_probability)
+        child2 = crossover(parent2, parent1, crossover_probability)
+        child1 = mutation(child1, mutation_probability)
+        child2 = mutation(child2, mutation_probability)
+        next_generation.append(child1)
+        next_generation.append(child2)
+    return next_generation
+
+
+# 8. 终止条件
+def is_termination_condition_met(iteration: int, iteration_times: int = ITERATION_TIMES) -> bool:
+    """
+    终止条件
+    :param iteration: 当前迭代次数
+    :param iteration_times: 总迭代次数
+    """
+    return iteration >= iteration_times
+
+
+# 9. 主函数
+def GeneticAlgorithm(population_size: int, iteration_times: int, crossover_probability: float,
+                     mutation_probability: float) -> Individual:
+    """
+    遗传算法
+    :param population_size: 种群大小
+    :param iteration_times: 迭代次数
+    :param crossover_probability: 交叉概率
+    :param mutation_probability: 变异概率
+    """
     # 初始化种群
-    population = generate_initial_population(POPULATION_SIZE, ITEMS_NUM)
-    print('初始种群：')
-    for idx, individual in enumerate(population):
-        print(idx, individual)
+    population = generate_initial_population(population_size, ITEMS_NUM)
+    # 迭代
+    iteration = 0
+
+    # 随迭代次数增加，突变概率减少
+    mutation_probability = 1 - (1 - mutation_probability) * iteration / iteration_times
+    best_fitness = 0
+    while best_fitness != 13692887:
+        population = next_generation(population, crossover_probability, mutation_probability)
+        # 计算最大适应度
+        fitness = [fitness_function(individual) for individual in population]
+        best_fitness = np.max(fitness)
+        print('iteration: {0}, max fitness: {1}'.format(iteration, best_fitness))
+        iteration += 1
+    # 计算适应度
+    fitness = [fitness_function(individual) for individual in population]
+    # 选择最优个体
+    best_individual = population[np.argmax(fitness)]
+    return best_individual
+
+
+if __name__ == '__main__':
+    best_individual = GeneticAlgorithm(POPULATION_SIZE, ITERATION_TIMES, CROSSOVER_PROBABILITY, MUTATION_PROBABILITY)
+    print(best_individual)
